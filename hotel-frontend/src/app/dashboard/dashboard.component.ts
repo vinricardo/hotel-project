936b84325fa69fcc25c5cedfa44281de56ac1e25
Vinicius Ricardo <vinicius.01deagosto@gmail.com>
2022-02-13 04:52:56 -0300

    feature: list of hotels

--- hotel-frontend/src/app/dashboard/dashboard.component.ts
+++ hotel-frontend/src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  hotelsTabs: string[]= [
+    'Most Popular',
+    'Special Ofers',
+    'Near Me'
+  ]
+  activeTab: string = this.hotelsTabs[0];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setTab(tab: string){
+    this.activeTab = tab;
+  }
+
+  checkActiveTab(tab: string):boolean{
+    return this.activeTab != tab;
   }
 
 }
--- hotel-frontend/src/shared/shared.module.ts
+++ hotel-frontend/src/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CardLodgingAvailableComponent } from './card-lodging-available/card-lodging-available.component';
+import { CardLodgingOfersComponent } from './card-lodging-ofers/card-lodging-ofers.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
 
@@ -9,11 +10,12 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 @NgModule({
   declarations: [
     SidenavComponent,
-    CardLodgingAvailableComponent
+    CardLodgingAvailableComponent,
+    CardLodgingOfersComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [SidenavComponent,CardLodgingAvailableComponent]
+  exports: [SidenavComponent,CardLodgingAvailableComponent,CardLodgingOfersComponent]
 })
 export class SharedModule { }
