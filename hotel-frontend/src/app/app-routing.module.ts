5d7c368a820be5bb580d99153b326d97eacf16b1
Vinicius <vinicius.01deagosto@gmail.com>
2022-04-21 13:50:51 -0300

    feature: finished section 'Explore City'

--- hotel-frontend/src/app/app-routing.module.ts
+++ hotel-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)}
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'explore',
+    loadChildren: () =>
+      import('./exploration/exploration.module').then(
+        (m) => m.ExplorationModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- hotel-frontend/src/shared/map/map.component.ts
+++ hotel-frontend/src/shared/map/map.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import * as leaflet from 'leaflet';
+import { tap } from 'rxjs';
+import { DashboardService } from 'src/app/dashboard/service/dashboard.service';
+
+import { Hotel } from './../models/hotel.model';
 
 @Component({
   selector: 'app-map',
@@ -9,28 +13,60 @@ import * as leaflet from 'leaflet';
 export class MapComponent implements OnInit, OnChanges {
   private map!: leaflet.Map;
   @Input() options: any;
-  constructor() {}
+  @Input() markers!: boolean;
+  @Input() coordHotel!: any[];
+  constructor(private dashboardService: DashboardService) {}
 
-  initMap(): void {
-    this.map = leaflet.map('map', this.options);
-    const tiles = leaflet.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      {
-        maxZoom: 18,
-        minZoom: 3,
-      }
-    );
-    tiles.addTo(this.map);
+  initMap() {
+    setTimeout(() => {
+      this.map = leaflet.map('map', this.options);
+      const tiles = leaflet.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          maxZoom: 18,
+          minZoom: 3,
+        }
+      );
+      tiles.addTo(this.map);
+    }, 100);
+  }
+
+  parseFloat(number: any) {
+    return parseFloat(number);
   }
 
   ngOnInit(): void {
     this.initMap();
+    this.addMarkers();
+  }
+
+  addMarkers() {
+    if (this.markers) {
+      setTimeout(() => {
+        this.dashboardService.hotels
+          .pipe(
+            tap((hotels: Hotel[]) => {
+              hotels.forEach((hotel) => {
+                leaflet
+                  .marker([
+                    this.parseFloat(hotel.location.longitude),
+                    this.parseFloat(hotel.location.latitude),
+                  ])
+                  .addTo(this.map)
+                  .bindPopup(`${hotel.name}`);
+              });
+            })
+          )
+          .subscribe();
+      }, 100);
+    }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (!changes['options'].firstChange) {
       this.map.remove();
       this.initMap();
+      this.addMarkers();
     }
   }
 }
